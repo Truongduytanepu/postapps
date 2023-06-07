3eb1bf189f87c75972e44fd2356566a481cf1623
Trương Duy Tân <truongduytan@MacBook-Pro-cua-Tan.local>
2023-06-07 10:11:26 +0700

    Hoanthanh

---
+++ Ex1Test/testLoginPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  testLoginPresenter.swift
+//  Ex1
+//
+//  Created by Trương Duy Tân on 05/06/2023.
+//
+
+import Foundation
+import Quick
+pod 'Quick'
+pod 'Nimble'
+pod 'MockingbirdFramework', '~> 0.20'
